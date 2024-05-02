f1035419a1c3b14f4dd091e656d77164ffa3e55f
jn <amyralmasy48@gmail.com>
2024-05-02 19:30:57 +0330

    animate modalbody province to city

--- client/src/app/organism/city-province-modal/city-province-modal.component.ts
+++ client/src/app/organism/city-province-modal/city-province-modal.component.ts
@@ -21,6 +21,24 @@ import { animate, style, transition, trigger } from '@angular/animations';
       ]),
       transition(':leave', [animate('300ms', style({ opacity: 0 }))]),
     ]),
+    trigger('flipInOut', [
+      transition(':enter', [
+        style({ transform: 'rotateY(-180deg)', opacity: 0 }),
+        animate('300ms', style({ transform: 'rotateY(0deg)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ transform: 'rotateY(180deg)', opacity: 0 })),
+      ]),
+    ]),
+    trigger('slideRightInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('300ms', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ transform: 'translateX(100%)', opacity: 0 })),
+      ]),
+    ]),
   ],
 })
 export class CityProvinceModalComponent implements OnInit {
